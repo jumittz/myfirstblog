22286d4d2e28f796a972840310e45e5ac907d44e
olawunmi <jumittz@gmail.com>
2020-04-21 02:55:04 +0100

    changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(g^_13cs6g&i#eg$+^cm4_f=nw1fo1$@=(o^ai5jz%y%ntde6u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere', 'jumittz.pythonanywhere.com']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog.apps.BlogConfig',     
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
